7556cdaa883110e54ce1e4b17593c2c0c5624fcf
Nasrudin Shaikh <nasrudin_shaikh@persistent.com>
2023-02-06 23:06:41 +0530

    removed unwanted variables

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "https://odyssey-lift-off-part5-server-production-d873.up.railway.app/", // change to YOUR own production server
-  cache: new InMemoryCache(),
-  name: "web",
-  version: "1.0",
+  cache: new InMemoryCache()
 });
 
 ReactDOM.render(
